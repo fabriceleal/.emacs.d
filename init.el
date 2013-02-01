d4c7d29da3a47d4f27c90f0541aaae8f50d6f7f5
Fabrice Leal <fabrice.leal.ch@gmail.com>
2013-02-01 12:23:46 -0800

    ignoric .elc files, installed package.el and installed haskell-mode

--- init.el
+++ init.el
@@ -5,3 +5,17 @@
 (add-to-list 'load-path "~/.emacs.d/go-mode")
 
 (load "go-mode")
+
+(load "package")
+
+(require 'package)
+
+(add-to-list 'package-archives
+						 '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(package-initialize)
+
+;(require 'package)
+;(add-to-list 'package-archives
+;             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;(package-initialize)
